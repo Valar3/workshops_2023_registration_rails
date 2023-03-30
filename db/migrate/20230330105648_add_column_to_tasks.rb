c798135632a437257c58bf2efe62c0ae8f3381c5
Wojciech Balon <balonw@gmail.com>
2023-03-30 23:38:18 +0200

    Done all tasks
    Successfully implemented date of completion param into views and into the 'new' form

--- db/migrate/20230330105648_add_column_to_tasks.rb
+++ db/migrate/20230330105648_add_column_to_tasks.rb
@@ -1,6 +1,5 @@
 class AddColumnToTasks < ActiveRecord::Migration[7.0]
   def change
-    add_column :tasks, :column_name, :column_type, :column_options
-    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+    add_column :tasks, :date_of_completion, :date
   end
 end
