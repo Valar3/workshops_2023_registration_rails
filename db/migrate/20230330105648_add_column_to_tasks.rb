3562899d205c7caca6d44a173cc4decbfbd66f59
Wojciech Balon <balonw@gmail.com>
2023-03-30 13:00:43 +0200

    Mid commit
    fixed the first two issues, and created a migration for the additional column for task no. 3

---
+++ db/migrate/20230330105648_add_column_to_tasks.rb
@@ -0,0 +1,6 @@
+class AddColumnToTasks < ActiveRecord::Migration[7.0]
+  def change
+    add_column :tasks, :column_name, :column_type, :column_options
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
